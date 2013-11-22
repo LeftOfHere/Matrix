f3c4afe664644bb5d233d23cd18be3ab146e0553
Zander <leftofhere@gmail.com>
2013-11-22 15:14:23 +0200

    Added matrix math file

--- Matrix/Matrices.cs
+++ Matrix/Matrices.cs
@@ -1,50 +1,177 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Matrix
 {
     class Matrices
     {
         //pre calculated value for pi/180
-        public const double _PI180 = 0.01745329251;
+        public const float _PI180 = 0.01745329251f;
         //pre calculated value for 180/pi
-        public const double _180PI = 57.2957795131;
+        public const float _180PI = 57.2957795131f;
 
-        public double DegreesToRadians(double x)
+        public float DegreesToRadians(float x)
         {
             return x * _PI180;
         }
 
-        public double RadiansToDegrees(double x)
+        public float RadiansToDegrees(float x)
         {
             return x * _180PI;
         }
 
-        public float[,] IdentityMatrix()
+        /// <summary>
+        /// |    1        0        0        0    |
+        /// |                                    |
+        /// |    0        1        0        0    |
+        /// |                                    |
+        /// |    0        0        1        0    |
+        /// |                                    |
+        /// |    0        0        0        1    |
+        /// </summary>
+        /// <returns>The matrix</returns>
+        public float[] IdentityMatrix()
         {
-            return new float[4, 4] { { 1f, 0f, 0f, 0f },
-                                      { 0f, 1f, 0f, 0f },
-                                      { 0f, 0f, 1f, 0f },
-                                      { 0f, 0f, 0f, 1f } };
+            float[] m = new float[16];
+
+            m[0] = m[5] = m[10] = m[15] = 1.0f;
+            m[1] = m[2] = m[3] = m[4] = 0.0f;
+            m[6] = m[7] = m[8] = m[9] = 0.0f;
+            m[11] = m[12] = m[13] = m[14] = 0.0f;
+
+            return m;
         }
 
-        public float[,] TranslationMatrix(float x, float y, float z)
+        /// <summary>
+        /// |    1        0        0        X    |
+        /// |                                    |
+        /// |    0        1        0        Y    |
+        /// |                                    |
+        /// |    0        0        1        Z    |
+        /// |                                    |
+        /// |    0        0        0        1    |
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        /// <returns>The matrix</returns>
+        public float[] TranslationMatrix(float x, float y, float z)
         {
-            return new float[4, 4] { { 1f, 0f, 0f, x },
-                                      { 0f, 1f, 0f, y },
-                                      { 0f, 0f, 1f, z },
-                                      { 0f, 0f, 0f, 1f } };
+            float[] matrix = IdentityMatrix();
+
+            matrix[12] = x;
+            matrix[13] = y;
+            matrix[14] = z;
+
+            return matrix;
         }
 
-        public float[,] ScaleMatrix(float x, float y, float z)
+        /// <summary>
+        /// |    SX       0        0        0    |
+        /// |                                    |
+        /// |    0        SY       0        0    |
+        /// |                                    |
+        /// |    0        0        SZ       0    |
+        /// |                                    |
+        /// |    0        0        0        1    |
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        /// <returns>The matrix</returns>
+        public float[] ScaleMatrix(float x, float y, float z)
         {
-            return new float[4, 4] { { x, 0f, 0f, 0f },
-                                      { 0f, y, 0f, 0f },
-                                      { 0f, 0f, z, 0f },
-                                      { 0f, 0f, 0f, 1f } };
+            float[] matrix = IdentityMatrix();
+
+            matrix[0] = x;
+            matrix[5] = y;
+            matrix[10] = z;
+
+            return matrix;
+        }
+
+        /// <summary>
+        /// |    1        0        0        0    |
+        /// |                                    |
+        /// |    0      cos(θ)   sin(θ)     0    |
+        /// |                                    |
+        /// |    0     -sin(θ)   cos(θ)     0    |
+        /// |                                    |
+        /// |    0        0        0        1    |
+        /// </summary>
+        /// <param name="degOrRad">THe degrees or radians to rotate</param>
+        /// <param name="rad">Set true if passing in radians.</param>
+        /// <returns>The matrix</returns>
+        public float[] RotateXMatrix(float degOrRad, bool rad = false)
+        {
+            if (!rad)
+                degOrRad = DegreesToRadians(degOrRad);
+
+            float[] matrix = IdentityMatrix();
+
+            // Rotate X formula.
+            matrix[5] = float.Parse(Math.Cos(degOrRad).ToString());
+            matrix[6] = -float.Parse(Math.Sin(degOrRad).ToString());
+            matrix[9] = -matrix[6];
+            matrix[10] = matrix[5];
+
+            return matrix;
+        }
+
+        /// <summary>
+        /// |  cos(θ)     0    -sin(θ)      0    |
+        /// |                                    |
+        /// |    0        1        0        0    |
+        /// |                                    |
+        /// |  sin(θ)     0     cos(θ)      0    |
+        /// |                                    |
+        /// |    0        0        0        1    |
+        /// </summary>
+        /// <param name="degOrRad"></param>
+        /// <param name="rad"></param>
+        /// <returns></returns>
+        public float[] RotateYMatrix(float degOrRad, bool rad = false)
+        {
+            if (!rad)
+                degOrRad = DegreesToRadians(degOrRad);
+
+            float[] matrix = IdentityMatrix();
+
+            // Rotate Y formula.
+            matrix[0] = float.Parse(Math.Cos(degOrRad).ToString());
+            matrix[2] = float.Parse(Math.Sin(degOrRad).ToString());
+            matrix[8] = -matrix[2];
+            matrix[10] = matrix[0];
+
+            return matrix;
+        }
+
+        /// <summary>
+        /// |  cos(θ)  -sin(θ)     0        0    |
+        /// |                                    |
+        /// |  sin(θ)   cos(θ)     0        0    |
+        /// |                                    |
+        /// |    0        0        1        0    |
+        /// |                                    |
+        /// |    0        0        0        1    |
+        /// </summary>
+        /// <param name="degOrRad"></param>
+        /// <param name="rad"></param>
+        /// <returns></returns>
+        public float[] RotateZMatrix(float degOrRad, bool rad = false)
+        {
+            if (!rad)
+                degOrRad = DegreesToRadians(degOrRad);
+
+            float[] matrix = IdentityMatrix();
+
+            // Rotate Z formula.
+            matrix[0] = float.Parse(Math.Cos(degOrRad).ToString());
+            matrix[1] = float.Parse(Math.Sin(degOrRad).ToString());
+            matrix[4] = -matrix[1];
+            matrix[5] = matrix[0];
+
+            return matrix;
         }
     }
 }
---
+++ Matrix/MatrixMath.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class MatrixMath
+    {
+        /// <summary>
+        /// Multiplies matrix m1 with matrix m2 (m1 x m2). 
+        /// </summary>
+        /// <param name="m1"></param>
+        /// <param name="m2"></param>
+        /// <param name="result"></param>
+        /// <returns>The resulting matrix.</returns>
+        public float[] MultiplyMatrix(float[] m1, float[] m2, float[] result)
+        {
+            // Fisrt Column
+            result[0] = m1[0] * m2[0] + m1[4] * m2[1] + m1[8] * m2[2] + m1[12] * m2[3];
+            result[1] = m1[1] * m2[0] + m1[5] * m2[1] + m1[9] * m2[2] + m1[13] * m2[3];
+            result[2] = m1[2] * m2[0] + m1[6] * m2[1] + m1[10] * m2[2] + m1[14] * m2[3];
+            result[3] = m1[3] * m2[0] + m1[7] * m2[1] + m1[11] * m2[2] + m1[15] * m2[3];
+
+            // Second Column
+            result[4] = m1[0] * m2[4] + m1[4] * m2[5] + m1[8] * m2[6] + m1[12] * m2[7];
+            result[5] = m1[1] * m2[4] + m1[5] * m2[5] + m1[9] * m2[6] + m1[13] * m2[7];
+            result[6] = m1[2] * m2[4] + m1[6] * m2[5] + m1[10] * m2[6] + m1[14] * m2[7];
+            result[7] = m1[3] * m2[4] + m1[7] * m2[5] + m1[11] * m2[6] + m1[15] * m2[7];
+
+            // Third Column
+            result[8] = m1[0] * m2[8] + m1[4] * m2[9] + m1[8] * m2[10] + m1[12] * m2[11];
+            result[9] = m1[1] * m2[8] + m1[5] * m2[9] + m1[9] * m2[10] + m1[13] * m2[11];
+            result[10] = m1[2] * m2[8] + m1[6] * m2[9] + m1[10] * m2[10] + m1[14] * m2[11];
+            result[11] = m1[3] * m2[8] + m1[7] * m2[9] + m1[11] * m2[10] + m1[15] * m2[11];
+
+            // Fourth Column
+            result[12] = m1[0] * m2[12] + m1[4] * m2[13] + m1[8] * m2[14] + m1[12] * m2[15];
+            result[13] = m1[1] * m2[12] + m1[5] * m2[13] + m1[9] * m2[14] + m1[13] * m2[15];
+            result[14] = m1[2] * m2[12] + m1[6] * m2[13] + m1[10] * m2[14] + m1[14] * m2[15];
+            result[15] = m1[3] * m2[12] + m1[7] * m2[13] + m1[11] * m2[14] + m1[15] * m2[15];
+
+            return result;
+        }
+
+        /// <summary>
+        // This is the arithmetical formula optimized to work with unit quaternions.
+        // |1-2y²-2z²        2xy-2zw         2xz+2yw       0|
+        // | 2xy+2zw        1-2x²-2z²        2yz-2xw       0|
+        // | 2xz-2yw         2yz+2xw        1-2x²-2y²      0|
+        // |    0               0               0          1|
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <param name="q">float[4] quaternian (x,y,z,w)</param>
+        /// <returns>The resultin matrix</returns>
+        public float[] QuaternianToMatrix(float[] matrix, float[] q)
+        {
+            // First Column
+            matrix[0] = 1 - 2 * (q[1] * q[1] + q[2] * q[2]);
+            matrix[1] = 2 * (q[0] * q[1] + q[2] * q[3]);
+            matrix[2] = 2 * (q[0] * q[2] - q[1] * q[3]);
+            matrix[3] = 0;
+
+            // Second Column
+            matrix[4] = 2 * (q[0] * q[1] - q[2] * q[3]);
+            matrix[5] = 1 - 2 * (q[0] * q[0] + q[2] * q[2]);
+            matrix[6] = 2 * (q[2] * q[1] + q[0] * q[3]);
+            matrix[7] = 0;
+
+            // Third Column
+            matrix[8] = 2 * (q[0] * q[2] + q[1] * q[3]);
+            matrix[9] = 2 * (q[1] * q[2] - q[0] * q[3]);
+            matrix[10] = 1 - 2 * (q[0] * q[0] + q[1] * q[1]);
+            matrix[11] = 0;
+
+            // Fourth Column
+            matrix[12] = 0;
+            matrix[13] = 0;
+            matrix[14] = 0;
+            matrix[15] = 1;
+
+            return matrix;
+        }
+    }
+}
