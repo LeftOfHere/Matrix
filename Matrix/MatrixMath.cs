d0225589382242e84ac7032c33351e2d144c28a5
Zander <leftofhere@gmail.com>
2013-11-22 15:30:19 +0200

    Added Quaternion Class

--- Matrix/MatrixMath.cs
+++ Matrix/MatrixMath.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Matrix
 {
@@ -13,10 +10,11 @@ namespace Matrix
         /// </summary>
         /// <param name="m1"></param>
         /// <param name="m2"></param>
-        /// <param name="result"></param>
         /// <returns>The resulting matrix.</returns>
-        public float[] MultiplyMatrix(float[] m1, float[] m2, float[] result)
+        public float[] MultiplyMatrix(float[] m1, float[] m2)
         {
+            float[] result = new Matrices().IdentityMatrix();
+
             // Fisrt Column
             result[0] = m1[0] * m2[0] + m1[4] * m2[1] + m1[8] * m2[2] + m1[12] * m2[3];
             result[1] = m1[1] * m2[0] + m1[5] * m2[1] + m1[9] * m2[2] + m1[13] * m2[3];
@@ -51,27 +49,28 @@ namespace Matrix
         // | 2xz-2yw         2yz+2xw        1-2x²-2y²      0|
         // |    0               0               0          1|
         /// </summary>
-        /// <param name="matrix"></param>
         /// <param name="q">float[4] quaternian (x,y,z,w)</param>
         /// <returns>The resultin matrix</returns>
-        public float[] QuaternianToMatrix(float[] matrix, float[] q)
+        public float[] QuaternianToMatrix(Quaternion q)
         {
+            float[] matrix = new Matrices().IdentityMatrix();
+
             // First Column
-            matrix[0] = 1 - 2 * (q[1] * q[1] + q[2] * q[2]);
-            matrix[1] = 2 * (q[0] * q[1] + q[2] * q[3]);
-            matrix[2] = 2 * (q[0] * q[2] - q[1] * q[3]);
+            matrix[0] = 1 - 2 * (q.Y * q.Y + q.Z * q.Z);
+            matrix[1] = 2 * (q.X * q.Y + q.Z * q.W);
+            matrix[2] = 2 * (q.X * q.Z - q.Y * q.W);
             matrix[3] = 0;
 
             // Second Column
-            matrix[4] = 2 * (q[0] * q[1] - q[2] * q[3]);
-            matrix[5] = 1 - 2 * (q[0] * q[0] + q[2] * q[2]);
-            matrix[6] = 2 * (q[2] * q[1] + q[0] * q[3]);
+            matrix[4] = 2 * (q.X * q.Y - q.Z * q.W);
+            matrix[5] = 1 - 2 * (q.X * q.X + q.Z * q.Z);
+            matrix[6] = 2 * (q.Z * q.Y + q.X * q.W);
             matrix[7] = 0;
 
             // Third Column
-            matrix[8] = 2 * (q[0] * q[2] + q[1] * q[3]);
-            matrix[9] = 2 * (q[1] * q[2] - q[0] * q[3]);
-            matrix[10] = 1 - 2 * (q[0] * q[0] + q[1] * q[1]);
+            matrix[8] = 2 * (q.X * q.Z + q.Y * q.W);
+            matrix[9] = 2 * (q.Y * q.Z - q.X * q.W);
+            matrix[10] = 1 - 2 * (q.X * q.X + q.Y * q.Y);
             matrix[11] = 0;
 
             // Fourth Column
@@ -81,6 +80,19 @@ namespace Matrix
             matrix[15] = 1;
 
             return matrix;
+        }
+
+        public float[] MultiplyQuaternions(Quaternion q1, Quaternion q2)
+        {
+            Quaternion newQ = new Quaternion();
+
+            // Assume that this multiplies q1 x q2, in this order, resulting in "newQ".
+            newQ.w = q1.w * q2.w - q1.x * q2.x - q1.y * q2.y - q1.z * q2.z;
+            newQ.x = q1.w * q2.x + q1.x * q2.w + q1.y * q2.z - q1.z * q2.y;
+            newQ.y = q1.w * q2.y - q1.x * q2.z + q1.y * q2.w + q1.z * q2.x;
+            newQ.z = q1.w * q2.z + q1.x * q2.y - q1.y * q2.x + q1.z * q2.w;
+
+            return result;
         }
     }
 }
---
+++ Matrix/Quaternion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    public class Quaternion
+    {
+        public float W;
+        public float X;
+        public float Y;
+        public float Z;
+
+        public Quaternion()
+        {
+        }
+
+        public Quaternion(float w, float x, float y, float z)
+        {
+            W = w;
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+}
