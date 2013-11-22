cc0b93c9c2bf905cc83fe342f43d5113a3a65fb7
LeftOfHere <leftofhere@gmail.com>
2013-11-22 10:25:23 +0200

    added .gitignore

---
+++ Matrix/Matrices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Matrices
+    {
+        //pre calculated value for pi/180
+        public const double _PI180 = 0.01745329251;
+        //pre calculated value for 180/pi
+        public const double _180PI = 57.2957795131;
+
+    }
+}
